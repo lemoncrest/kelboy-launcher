4a3770fe485c3d6cb06e68c5b369d0f2f69592be
Bit <bitstuffing@gmail.com>
2020-07-18 16:20:18 +0200

    initial commit

---
+++ core/menu.py
@@ -0,0 +1,80 @@
+import pygame
+from core.settings import *
+
+
+class MenuBoard(pygame.sprite.Sprite):
+
+    def __init__(self, game):
+        self.game = game
+        self.groups = game.all_sprites
+        self._layer = 1
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.Surface((width*0.9,height*0.9))
+        self.image.fill((0,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.centery = height / 2
+        self.rect.centerx = width / 2
+
+
+class MenuCursor(pygame.sprite.Sprite):
+
+    def __init__(self, menu, game, items):
+        self.items = items
+        self.menu = menu
+        self.game = game
+        self.groups = game.all_sprites
+        self._layer = 2
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.Surface((self.menu.board.rect.width * 0.97,
+                                self.menu.items.rect.height))
+        self.image.fill((0,0,255))
+        self.rect = self.image.get_rect()
+        self.rect.y = self.menu.items.menu_init_y + self.rect.height
+        self.rect.centerx = width / 2
+        self.selectedItem = 0
+
+    def down(self):
+        if self.selectedItem < len(self.menu.items.items) - 1:
+            self.rect.y += self.rect.height
+            self.selectedItem += 1
+
+    def up(self):
+        if self.selectedItem != 0:
+            self.rect.y -= self.rect.height
+            self.selectedItem -= 1
+
+    def select(self):
+        print(str(self.selectedItem))
+        print(self.items.items[self.selectedItem]["action"])
+
+
+class MenuItems(pygame.sprite.Sprite):
+
+    def __init__(self, menu, game):
+        self.menu = menu
+        self.game = game
+        self.groups = game.all_sprites
+        self._layer = 3
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.items = []
+        self.font = pygame.font.SysFont('Consolas', 30)
+        self.image = self.font.render('', False, (255,255,255))
+        self.rect = self.image.get_rect()
+        self.menu_init_y = 10
+        self.text_size_y = self.rect.height
+
+    def draw(self):
+        counter = 0
+        for item in self.items:
+            counter += 1
+            text_item = self.font.render(item["title"], False, (255,255,255))
+            text_item_rect = text_item.get_rect()
+            self.game.screen.blit(text_item, (self.menu.cursor.rect.left * 1.2, self.menu_init_y + (text_item_rect.height * counter)))
+
+
+class Menu(MenuBoard, MenuCursor, MenuItems):
+
+    def __init__(self, game):
+        self.board = MenuBoard(game)
+        self.items = MenuItems(self, game)
+        self.cursor = MenuCursor(self, game, self.items)
---
+++ main.py
@@ -0,0 +1,72 @@
+import pygame
+from core.settings import *
+from core.menu import Menu
+from core.colorpalette import *
+
+class Main:
+
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Menu')
+        self.rgb = RGBColors()
+        self.clock = pygame.time.Clock()
+        self.frameRate = frameRate
+        self.frameCounter = 0
+        self.running = True
+        self.loadAssets()
+
+    def loadAssets(self):
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.menu = Menu(self)
+        self.menu.items.items = [
+            {
+                "title" : "Launch Emulationstation",
+                "action" : "exit",
+                "external": "emulationstation"
+            },{
+                "title" : "Settings",
+                "action" : "menu",
+                "menu" : "settings"
+            },{
+                "title" : "Quit",
+                "action" : "exit",
+                "external": "sudo reboot"
+            }
+        ]
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                self.menu.cursor.down()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                self.menu.cursor.up()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                self.menu.cursor.select()
+
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(self.rgb.black)
+        self.all_sprites.draw(self.screen)
+        self.menu.items.draw()
+
+    def run(self):
+        while self.running:
+            self.frameCounter += 1
+            self.clock.tick(self.frameRate)
+            self.events()
+            self.update()
+            self.draw()
+            pygame.display.flip()
+            if self.frameCounter == self.frameRate:
+                self.frameCounter = 0
+
+main = Main()
+while main.running:
+    main.run()
+pygame.quit()

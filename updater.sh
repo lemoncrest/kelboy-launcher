371c34cd4e927571fc72efa3f2174dc2d13bcc69
Bit <bitstuffing@gmail.com>
2020-07-23 14:46:06 +0200

    git method for update

--- updater.sh
+++ updater.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-python3 updater.py
+#python3 updater.py
+#cd /home/pi
+#echo "replacing..."
+#rm -Rf kelboy-launcher
+#mv /tmp/kelboy-launcher-master kelboy-launcher
+#echo "done!"
+#cd /home/pi/kelboy-launcher
+#chmod +x updater.sh
+#echo "launching new revision..."
+#python3 main.py &
+
 cd ..
-echo "replacing..."
 rm -Rf kelboy-launcher
-mv /tmp/kelboy-launcher-master kelboy-launcher
-echo "done!"
+git clone https://github.com/lemoncrest/kelboy-launcher
 cd kelboy-launcher
 chmod +x updater.sh
-echo "launching new revision..."
-python3 main.py &
+python3 main.py

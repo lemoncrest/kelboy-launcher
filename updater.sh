8f5f1402f16635e9684e6e94e95d426444ed7f6b
bitstuffing <eduardo.higuero@hotmail.com>
2020-07-23 14:16:47 +0200

    updater (WIP)

--- updater.sh
+++ updater.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 python updater.py
+#mv /tmp/kelboy-launcher-master /tmp/kelboy-launcher
+cd ..
+echo "replacing..."
+mv /tmp/kelboy-launcher kelboy-launcher
+echo "done!"
+cd kelboy-launcher
+echo "launching new revision..."
+python main.py &

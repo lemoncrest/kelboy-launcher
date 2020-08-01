668d2feadfbc933d9e43b18fced1b0ee680af307
Bit <bitstuffing@gmail.com>
2020-08-01 14:03:12 +0200

    ups

--- core/wifi.py
+++ core/wifi.py
@@ -21,7 +21,7 @@ class Wifi():
         output = process.stdout
         logger.debug("output is: %s" % output)
         networks = []
-        for line in output.split("/n"):
+        for line in output.split("\n"):
             if '""' not in line and '"' in line:
                 logger.debug("using line %s " % line)
                 name = line.split('"')[1].split('"')[0]

5a7f05bc659b106643fa2fe80deac18b15224263
bitstuffing <eduardo.higuero@hotmail.com>
2020-08-04 17:59:50 +0200

    testing keypad commands...

--- bin/keypad.sh
+++ bin/keypad.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+
+function joy(){
+  jstest --event /dev/input/js0
+}
+
 while true; do
-	result=jstest --event /dev/input/js0 | grep -m 1 "type 1, time .*, number .*, value 1"
-  echo $result
-  sleep 0.01
+	joy 1>joystick.log
+  sleep 0.05
+  PID=$(ps -e | grep jstest | cut -c 1-5) >/dev/null 2>&1
+  sudo kill -9 $PID 1>/dev/null
+  MATCH=$(grep -w "${GRAB}" joystick.log | cut -c 32-) >/dev/null 2>&1
+  case $MATCH in
+    "number 11, value 1" ) echo "UP" ;;
+    "number 11, value 0" ) echo "UP released" ;;
+    "number 10, value 1" ) echo "DOWN" ;;
+    "number 10, value 0" ) echo "DOWN released" ;;
+    "number 9, value 1" ) echo "LEFT" ;;
+    "number 9, value 0" ) echo "LEFT released" ;;
+    "number 8, value 1" ) echo "RIGHT" ;;
+    "number 8, value 0" ) echo "RIGHT released" ;;
+  esac
 done
+rm joystick.log

6e3c86baabb88df2e69fe0349ea47c546ba1c4b1
bitstuffing <eduardo.higuero@hotmail.com>
2020-08-04 11:52:07 +0200

    battery script (WIP)

---
+++ bin/battery.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+status='cat /sys/class/power_supply/max1726x_battery/capacity'
+
+if [ $status>0 ]
+then
+  if [ $status > 25 ]
+  then
+    level="25"
+    if [ $status > 50 ]
+    then
+      level="50"
+      if [ $status > 75 ]
+      then
+        level="75"
+        if [ $status > 95 ]
+        then
+          level="95"
+        fi
+      fi
+    else
+    fi
+  fi
+else
+  level="0"
+fi
+
+'./bin/pngview resources/graphics/battery-$level.png -b 0 -l 300003 -x 300 -y 5'
+
+echo "done!"

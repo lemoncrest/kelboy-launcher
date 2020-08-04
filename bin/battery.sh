8c6efdde583986e26aebe8ae60c94cf6e0d84792
bitstuffing <eduardo.higuero@hotmail.com>
2020-08-04 12:03:30 +0200

    tests

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exitCode=$(cat /sys/class/power_supply/max1726x_battery/capacity)
-
+status=$(cat /sys/class/power_supply/max1726x_battery/capacity)
+level = "0"
 if [ $status>0 ]
 then
   if [ $status > 25 ]
@@ -20,10 +20,8 @@ then
       fi
     fi
   fi
-else
-  level="0"
 fi
 
-exitCode=$("./pngview ../resources/graphics/battery-$level.png -b 0 -l 300003 -x 300 -y 5")
+exitCode=$("./pngview '../resources/graphics/battery-$level.png' -b 0 -l 300003 -x 300 -y 5")
 
 echo "done!"

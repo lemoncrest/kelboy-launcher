a68a7b03ea11481e86a0a06dfa0ab2130259127d
bitstuffing <eduardo.higuero@hotmail.com>
2020-08-05 10:25:31 +0200

    testing updater

--- launcher.sh
+++ launcher.sh
@@ -6,6 +6,7 @@ rm bin/battery.old
 cd bin
 ./battery.sh &
 cd ..
+python3 joystick.py &
 #exitCode=$(python3 main.py)
 exitCode=$(python3 main.py 2>&1 )
 status=$?
@@ -14,6 +15,8 @@ if [ $status>0 ]
 then
   if [ $status == 10 ]
   then
+    sudo killall -9 battery.sh
+    sudo killall -9 pngview
     foo="`cat command`"
     $foo 2>&1 &
     wait

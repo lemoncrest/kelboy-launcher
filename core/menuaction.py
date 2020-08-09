b40531ce892b3aeedad38f1a95538f53f5bc37c3
bitstuffing <eduardo.higuero@hotmail.com>
2020-08-09 11:44:45 +0200

    wifi selection and stored in wifi.json menu settings

--- core/menuaction.py
+++ core/menuaction.py
@@ -39,6 +39,59 @@ def saveWifiConfig(params=[]):
     wifi = Wifi()
     wifi.buildWpaSupplicantAndConnect(ssid=ssid,pwd=pwd)
 
+def saveWifiSSID(params=[]):
+    '''
+    {
+        "action": "param",
+        "external": "text-config",
+        "name": "ssid",
+        "title": "Network name",
+        "value": "",
+        "visible": "true"
+    }
+    '''
+    '''
+    with open(os.path.join("resources/menus/wifi.json")) as jsonMenu:
+        menu = json.load(jsonMenu)
+        for element in menu:
+            if "name" in element and element["name"] == self.lastMenuParam:
+                element["value"] = buffer
+
+    with open(os.path.join(os.getcwd(),"resources/menus/wifi.json"),"w") as jsonMenu:
+        json.dump(menu, jsonMenu, indent=4, sort_keys=True)
+    '''
+    ssid=''
+    logger.debug(str(type(params)))
+    if type(params) is list:
+        logger.debug("list")
+        for element in params:
+            logger.debug("ele %s" % str(element))
+            if "ssid" in element:
+                logger.debug("%s" % str(element))
+                ssid = element["ssid"]
+                logger.debug("target ssid %s" % (ssid))
+    if len(ssid)>0:
+        with open(os.path.join("resources/menus","wifi.json")) as jsonMenu:
+            settings = json.load(jsonMenu)
+        for element in settings:
+            if 'name' in element and element["name"] == 'ssid':
+                element["value"] = ssid
+        #now save settings
+        with open(os.path.join(os.getcwd(),"resources/menus/wifi.json"),"w") as jsonMenu:
+            json.dump(settings, jsonMenu, indent=4, sort_keys=True)
+        logger.debug("saved!")
+        #TODO launch password keyboard
+        '''
+        element = {}
+        element["external-keyboard"] = "saveWifiPWD"
+        element["params"] = [{
+            'pwd': element["title"]
+        }]
+        return element
+        '''
+        return settings
+
+
 def scanWifi(params=[]):
     wifi = Wifi()
     networks = wifi.scan_networks()
@@ -50,7 +103,7 @@ def scanWifi(params=[]):
         element = {}
         element["title"] = "%s" % (networkName)
         element["action"] = "function"
-        element["external"] = 'saveWifiConfig'
+        element["external"] = 'saveWifiSSID'
         element["params"] = [{
             'ssid': element["title"]
         }]
@@ -123,7 +176,7 @@ def connectBluetooth(params=[]):
 
     bl.exit()
 
-def loadRoms(params=[]):
+def loadRoms(params=[]): #TODO launch emulationstation configurations by path
     menu = []
     if "type" not in params:
         dir= os.listdir(ROMS_PATH)

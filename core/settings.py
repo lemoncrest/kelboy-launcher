bbc69f5932cee7c5cb6c024525894189c5e864f5
bitstuffing <eduardo.higuero@hotmail.com>
2020-09-09 14:30:46 +0200

    charging detection and new icons (WIP)

--- core/settings.py
+++ core/settings.py
@@ -27,3 +27,7 @@ FRAMES_IN = 16
 FRAMES_OUT = 16
 
 BARSIZE = 25
+
+BATTERY_PERCENTAGE_CMD = 'cat /sys/class/power_supply/max1726x_battery/capacity'
+
+FUELGAUGE_CURRENT_CMD = 'cat /sys/class/power_supply/max1726x_battery/current_now'

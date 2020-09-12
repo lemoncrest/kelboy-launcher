9b03db72f7d15fb5195bcec9c697118ccdad78c9
Bit <bitstuffing@gmail.com>
2020-09-12 19:20:04 +0200

    brightness finished (at least for joystick)

--- joystick.py
+++ joystick.py
@@ -243,11 +243,11 @@ while True:
         if button_states["SELECT"] and button_states["DOWN"]:
             logger.debug("bundle2 down detected")
         if button_states["SELECT"] and button_states["LEFT"]:
-            lightLevel = lightLevel - 15 if lightLevel >= 15 else 0
+            lightLevel = lightLevel - 10 if lightLevel >= 10 else 0
             brightness.ChangeDutyCycle(lightLevel)
             logger.debug("brightness is %s" % lightLevel)
         if button_states["SELECT"] and button_states["RIGHT"]:
-            lightLevel = lightLevel + 15 if lightLevel <= 85 else 100
+            lightLevel = lightLevel + 10 if lightLevel <= 90 else 100
             brightness.ChangeDutyCycle(lightLevel)
             logger.debug("brightness is %s" % lightLevel)
 

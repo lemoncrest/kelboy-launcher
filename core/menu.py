df60f0965b7aa56828a8a8111743ef634092b1aa
Bit <bitstuffing@gmail.com>
2020-09-13 20:44:47 +0200

    wifi fix finished

--- core/menu.py
+++ core/menu.py
@@ -588,13 +588,14 @@ class MenuStatus(pygame.sprite.Sprite):
         for i in range(0,bars+1,1):
             xP = x + self.padding * (i+1) * 2 + (barWidth*i) + self.margin
             ySize = int(barHeight / totalBars * i) - (self.padding*i*2)
-            if ySize < 1:
-                ySize = 1
+#            if ySize < 1:
+#                ySize = 1
             yP = int(BARSIZE) - (ySize) - 5
-            if yP < 1:
-                yP = 1
-            rect = pygame.Rect(xP, yP, barWidth, ySize)
-            pygame.draw.rect(self.image, GREEN, rect)
+#            if yP < 1:
+#                yP = 1
+            if yP > 0:
+                rect = pygame.Rect(xP, yP, barWidth, ySize)
+                pygame.draw.rect(self.image, GREEN, rect)
         for i in range(bars,totalBars,1):  # points
             txt = self.font.render(".", True, WHITE)
             xP = x + self.padding * (i+1) * 2 + (barWidth*i) + self.margin + (self.font.size("X")[0]/2)

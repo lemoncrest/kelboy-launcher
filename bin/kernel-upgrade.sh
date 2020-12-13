2af68d7073ee7dee8a10d04a97c7247f233a56e5
bitstuffing <eduardo.higuero@hotmail.com>
2020-12-13 15:42:28 +0000

    (WIP) script for upgrade/reinstall kernels

--- bin/kernel-upgrade.sh
+++ bin/kernel-upgrade.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 cd /tmp
-git clone https://github.com/lemoncrest/Kelboy2.X_Drivers kernels
+#git clone https://github.com/lemoncrest/Kelboy2.X_Drivers kernels
 
 cd kernels
 
-printed=0
-keep=''
+printed=1
+#array
+declare -a array=()
+
+keep='Kelboy'
 for dir in */ ; do
-	if [[ ${} == ${} ]] ; then
-		if (( !printed)) ; then
-			echo "$keep"
-			printed=1
-		fi
-	else
-		printed=0
-		keep=$dir
+	if [[ ${dir:0:5} == ${keep:0:5} ]] ; then
+		#echo "$printed: $dir"
+		array[$((printed-1))]=${dir::-1}
+		printed=$((printed+1))
+	#else
+		#printed=0
+		#keep=$dir
+		#echo "discarting $dir"
 	fi
 done
-
-
+if [[ ${#array[@]} -gt 0 ]] ; then
+	j=1
+	echo "choose which kernel do you want to install: "
+	for i in "${array[@]}"
+	do
+		echo "$j) $i"
+	done
+	
+else
+	echo "No kernels detected"
+fi

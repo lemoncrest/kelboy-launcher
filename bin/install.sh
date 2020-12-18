9bc137392a750ccd42eeec4c4770e4f88a043a7d
bit <bitstuffing@gmail.com>
2020-12-18 13:05:44 +0100

    install and pillow in dev revision

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-sudo apt install -y git python3-setuptools python3-pygame python3-bluez python3-pexpect python3-evdev python3-psutil libpng12-0 bsdtar
-
-cd /tmp
-
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py
-
-sudo pip3 install Pillow
+sudo apt install -y git python3-willow python3-pygame python3-bluez python3-pexpect python3-evdev python3-psutil libpng12-0 bsdtar

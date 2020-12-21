fa2035dd78eb84cfb3c00a9a8d65d8f6b59ef76b
bit <bitstuffing@gmail.com>
2020-12-21 10:30:30 +0100

    tests for time.sleep in draw items (menu)

--- core/menu.py
+++ core/menu.py
@@ -493,7 +493,7 @@ class MenuItems(pygame.sprite.Sprite):
 #                            logger.debug("discarting %s" % str(counter))
 
                 counter += 1
-                time.sleep(0.005)
+                time.sleep(0.015)
 
 
 class MenuStatus(pygame.sprite.Sprite):

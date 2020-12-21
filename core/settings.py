6fdd53294560d9daa0f7f8ef787db9a2caf1afaa
bitstuffing <eduardo.higuero@hotmail.com>
2020-12-21 10:55:24 +0100

    MENU_ITEM_REFRESH_TIME in settings to control sleep in menu items draw

--- core/settings.py
+++ core/settings.py
@@ -80,6 +80,7 @@ FONT_TYPE_KEYBOARDBAR = getValue("FONT_TYPE_KEYBOARDBAR",'resources/fonts/DejaVu
 WPA_SUPPLICANT = getValue("WPA_SUPPLICANT",'/etc/wpa_supplicant/wpa_supplicant.conf','where is the wpa-supplicant in the system, must not be changed')
 UPLOAD_SITE = getValue("UPLOAD_SITE",'https://github.com/lemoncrest/kelboy-launcher/archive/master.zip','default github download repository like zip')
 MAX_MENU_ITEMS = getValue("MAX_MENU_ITEMS",10,'max number of items in the menus')
+MENU_ITEM_REFRESH_TIME = getValue("MENU_ITEM_REFRESH_TIME",0.002,"Menu item refresh time, higher better performance")
 TEXT_MOVEMENT_EFFECT = getValue("TEXT_MOVEMENT_EFFECT",False,"Movement effect in menu for long texts")
 BACKGROUND_ENABLE = getValue("BACKGROUND_ENABLE",False,"Background enable in launcher menu")
 BACKGROUND_PICTURE = getValue("BACKGROUND_PICTURE","background-br.png",'this value configures the background image and must be stored in resources/graphics/')
